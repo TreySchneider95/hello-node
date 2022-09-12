5ded1b4168952870cf50e10ca974ecd5ecfc804d
TreySchneider95 <tschneid95@gmail.com>
2022-09-12 12:48:27 -0600

    tested for loop solution

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 console.log("Hello World")
 
-console.log("HH   HH        lll lll                                  lll      dd !!!")
-console.log("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!")
-console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!")
-console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd    ")
-console.log("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!")                                                                   
+// console.log("HH   HH        lll lll                                  lll      dd !!!")
+// console.log("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!")
+// console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!")
+// console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd    ")
+// console.log("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!")         
+
+
+
+let megaHello = [["HH   HH        lll lll                                  lll      dd !!!"],
+["HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!"],
+["HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!"],
+["HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd    "],
+["HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!"]]
+
+for (const arr of megaHello){
+    console.log(arr[0])
+}
