7b3d7d2c20a480eabd85b38102c2510c840e5bf3
TreySchneider95 <tschneid95@gmail.com>
2022-09-12 12:58:59 -0600

    butilt function for large word

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ let megaHello = [["HH   HH        lll lll                                  lll  
 ["HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd    "],
 ["HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!"]]
 
-for (const arr of megaHello){
-    console.log(arr[0])
+let megaText = [[" ____    _             _                   _   "],
+["| __ )  (_)   __ _    | |_    ___  __  __ | |_ "],
+["|  _/   | |  / _` |   | __|  / _ | | |/ / | __|"],
+["| |_)   | | | (_| |   | |_  |  __/  >  <  | |_ "],
+["|____|  |_| |__,  |   |__|  |___| /_/|_|  |__|"],
+["              |___/                             ]"]]
+
+function megaWordPrinter(word){
+    for (const arr of word){
+        console.log(arr[0])
+    }
 }
+
+megaWordPrinter(megaHello)
+megaWordPrinter(megaText)
